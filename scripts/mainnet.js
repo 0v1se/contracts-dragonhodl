40699890dfc449f72e045d0a261ea7e2031dccff
0v1se <dev@0v1se.com>
2018-02-26 11:51:00 +0300

    temp

--- scripts/mainnet.js
+++ scripts/mainnet.js
@@ -1,3 +1,3 @@
-DragonToken.new("200000000000000000", 1000, "DragonHODL", "DHRARE");
+DragonToken.new("200000000000000000", 10000, "DragonHODL", "DHRARE");
 DragonToken.new("2000000000000000000", 100, "DragonHODL", "DHEPIC");
-DragonToken.new("20000000000000000000", 3, "DragonHODL", "DHLEGEND");
+DragonToken.new("20000000000000000000", 10, "DragonHODL", "DHLEGEND");
