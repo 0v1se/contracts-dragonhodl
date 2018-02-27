e0689de320e77c7ffa39dec04d1eff3ea383b47c
0v1se <dev@0v1se.com>
2018-02-27 13:15:38 +0300

    fixes

--- truffle.js
+++ truffle.js
@@ -1,5 +1,3 @@
-var TrezorProvider = require("@daonomic/trezor-web3-provider");
-
 module.exports = {
   networks: {
     development: {
@@ -8,7 +6,9 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-      provider: () => TrezorProvider("http://ether-dev.roborox.ru:8545", "m/44'/1'/0'/0/0"),
+      provider: () => {
+        return require("@daonomic/trezor-web3-provider")("http://ether-dev:8545", "m/44'/1'/0'/0/0");
+      },
       network_id: 3,
       from: "0x755d601426add61e8597c1eecf98a8c49247d723",
       gas: 2000000,
