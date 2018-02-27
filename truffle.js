618814494a4542a12a6d35949b3528995e0209ee
0v1se <dev@0v1se.com>
2018-02-27 12:41:58 +0300

    update trezor

--- truffle.js
+++ truffle.js
@@ -1,58 +1,24 @@
-var network;
-for(var i=0; i < process.argv.length; i++) {
-    if (process.argv[i].startsWith("--network=")) {
-        network = process.argv[i].substring(10);
-    }
-}
-
-function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-}
+var TrezorProvider = require("@daonomic/trezor-web3-provider");
 
 module.exports = {
-    networks: {},
-    solc: {
-        optimizer: {
-            enabled: true,
-            runs: 200
-        }
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: () => TrezorProvider("http://ether-dev.roborox.ru:8545", "m/44'/1'/0'/0/0"),
+      network_id: 3,
+      from: "0x755d601426add61e8597c1eecf98a8c49247d723",
+      gas: 2000000,
+	  gasPrice: 3000000000
     }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
 };
-
-var networkConfig = {
-    gas: 1100000,
-    gasPrice: 3000000000
-};
-
-if (network) {
-	var ProviderEngine = require("web3-provider-engine");
-	var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-	var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-	var Web3 = require("web3");
-	var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js')
-	var Wallet = require("ethereumjs-wallet");
-
-	function createEngine(url, wallet) {
-	    var engine = new ProviderEngine();
-	    engine.addProvider(new WalletSubprovider(wallet, {}));
-	    engine.addProvider(new FilterSubprovider());
-	    engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(url)));
-	    engine.on('error', function(err) {
-	        console.error(err.stack)
-	    });
-	    return engine;
-	}
-
-	var configName = getUserHome() + "/.ethereum/" + network + ".json";
-    console.log("using config file: " + configName);
-    var json = require(configName);
-    var wallet = Wallet.fromPrivateKey(new Buffer(json.key, "hex"));
-    var engine = createEngine(json.url, wallet);
-    engine.start();
-    networkConfig.from = wallet.getAddressString();
-    console.log("address: " + wallet.getAddressString());
-    networkConfig.network_id = json.network_id;
-    networkConfig.provider = engine;
-
-    module.exports.networks[json.name] = networkConfig;
-}
